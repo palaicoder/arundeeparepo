5df86e679fde71926809dac4865f90cb4e1261db
Arun Mathew <arun1math@gmail.com>
2022-02-13 15:41:57 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import json
+import csv
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    headers = set([])
+# opening JSON file and loading the date
+#into the  variable date
+
+    with open(data.json) as json_file:
+        data = json.load(json_file)
